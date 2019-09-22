58aa11e89b3433e07ee169947cd47f13c94bdcc2
therohitjha <therohitjha@gmail.com>
2019-09-22 15:24:38 +0530

    initial commit

--- src/SingleNews.js
+++ src/SingleNews.js
@@ -2,16 +2,15 @@ import React from 'react'
 
 const SingleNews=({item})=>{
 return(
-    <div className="col s12 m6">
-      <div className="card" style={{}}>
+    <div className="col s12 m6 l4">
+      <div className="card">
         <div className="card-image">
           <img src={item.urlToImage} alt={item.title}/>
         </div>
         <div className="card-content">
           <p className='center-align'>{item.title}</p>
-          
         </div>
-        <div className="card-action">
+        <div className="card-action center-align" >
           <a href={item.url} target='_blank' rel="noopener noreferrer">Full News</a>
         </div>
       </div>
