df149799d743dd58d185698416445cf1d6052229
therohitjha <therohitjha@gmail.com>
2019-09-22 14:26:38 +0530

    Updates

---
+++ precache-manifest.957120d2c86c75fe4d64d50decc11893.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "236647dbf923640b36dd1a55d63ee663",
+    "url": "/therohitjha-news/index.html"
+  },
+  {
+    "revision": "6013425b676e1ce70df2",
+    "url": "/therohitjha-news/static/js/2.4680f36c.chunk.js"
+  },
+  {
+    "revision": "0ebe3b48b41cd7b6961c",
+    "url": "/therohitjha-news/static/js/main.d209ece6.chunk.js"
+  },
+  {
+    "revision": "7fee2de4c2bc47d7feb6",
+    "url": "/therohitjha-news/static/js/runtime-main.01a6206f.js"
+  }
+]);
